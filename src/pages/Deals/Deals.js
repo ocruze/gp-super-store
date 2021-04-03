b053292072686a9e6eec6d993e46f0033ec2b709
ocruze <33525693+ocruze@users.noreply.github.com>
2021-04-04 01:07:57 +0200

    adding pages: home, deals and cart

---
+++ src/pages/Deals/Deals.js
@@ -0,0 +1,13 @@
+import "./Deals.scss";
+
+import React, { Component } from "react";
+
+export default class Deals extends Component {
+  render() {
+    return (
+      <div className="page-container">
+        <h1>This is the deals page</h1>
+      </div>
+    );
+  }
+}
